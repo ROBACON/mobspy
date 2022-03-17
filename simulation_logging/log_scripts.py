bbe6aa941ddd2e4e5c6f98fe66b25cf58243e611
Thomas Nowak <thomas.nowak@lri.fr>
2022-03-17 12:23:00 +0100

    logging to stderr instead of stdout

--- simulation_logging/log_scripts.py
+++ simulation_logging/log_scripts.py
@@ -1,21 +1,23 @@
 from copy import deepcopy
 
+import sys
+
 
 def error(message):
-    print('\033[91m' + 'ERROR: ' + message + '\033[0m')
+    print('\033[91m' + 'ERROR: ' + message + '\033[0m', file=sys.stderr)
     exit(1)
 
 
 def debug(message=''):
     message_copy = deepcopy(message)
-    print(message_copy.replace('_dot_', '.'))
+    print(message_copy.replace('_dot_', '.'), file=sys.stderr)
     pass
 
 
 def warning(message):
     message_copy = deepcopy(message)
     message.replace('_dot_', '.')
-    print('\033[91m' + 'WARNING: ' + message_copy + '\033[0m')
+    print('\033[91m' + 'WARNING: ' + message_copy + '\033[0m', file=sys.stderr)
 
 
 if __name__ == '__main__':
