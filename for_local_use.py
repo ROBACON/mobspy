670e80b74e1572223a99abf5198343235b80c264
fabriciocravo <fabricio.cravo@student.ecp.fr>
2024-07-15 16:32:15 +0200

    All tests passed localy, performing github check

--- for_local_use.py
+++ for_local_use.py
@@ -8,11 +8,7 @@ from numpy import issubdtype, floating, integer, array
 
 if __name__ == '__main__':
 
-    idk = array([1, 2, 3])
-
-    for a in idk:
-        print(issubdtype(a, floating))
-        print(issubdtype(a, integer))
+    pass
 
 
 
