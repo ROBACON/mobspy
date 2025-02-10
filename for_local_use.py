0a18615e3f908b660f7623e17b78b2b291463358
Fabricio Cravo <fabricio.cravo1@outlook.com>
2025-02-10 09:45:14 -0500

    Fixed typo in BioNetGen Comparison model

--- for_local_use.py
+++ for_local_use.py
@@ -3,6 +3,12 @@ from mobspy import modules
 
 if __name__ == '__main__':
 
-    pass
+    Spe_Dict = {'E_1':None, 'Eb_1':None, 'D_2': None}
 
+    L = BaseSpecies(list(Spe_Dict.keys()))
+
+    for spe in L:
+        Spe_Dict[spe.get_name()] = spe
+
+    print(Spe_Dict)
 
