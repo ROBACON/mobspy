ae895e698a6e6519d7bef58c6fa8ce610f489078
Thomas Nowak <thomas@thomasnowak.net>
2025-02-17 16:43:09 +0100

    Fixed typo

--- docs/example_models/journal_models/BioNetGen_Comparison.py
+++ docs/example_models/journal_models/BioNetGen_Comparison.py
@@ -1,23 +1,22 @@
 from mobspy import *
 
-R0, L0, A0, kon, koff, kAon, kAoff, kAp, kAdp = ModelParameters(100, 500, 100, 0.01,
-                                                                0.1, 0.01, 0.1, 0.01, 0.1)
+R0, L0, A0, kon, koff, kAon, kAoff, kAp, kAdp = ModelParameters(
+    100, 500, 100, 0.01, 0.1, 0.01, 0.1, 0.01, 0.1
+)
 
 r_link, C = BaseSpecies()
 r_link.r_0, r_link.r_1
 R, L, A = New(r_link)
 
-R.r_0 + L.r_0 >> R.r_1 + L.r_1 [kon, koff]
+R.r_0 + L.r_0 >> R.r_1 + L.r_1[kon, koff]
 
-R.a_0 + A.r_0 >> R.a_1 + A.r_1 [kAon, kAoff]
+R.a_0 + A.r_0 >> R.a_1 + A.r_1[kAon, kAoff]
 
-C.assing(A.r_1.n_p - R.a_1.r_0)
-C + A.n_p.r_1 >> C + A.y_p.r_1 [lambda c: kAp*c]
+C.assign(A.r_1.n_p - R.a_1.r_0)
+C + A.n_p.r_1 >> C + A.y_p.r_1[lambda c: kAp * c]
 
-A.y_p >> A.n_p [kAdp]
+A.y_p >> A.n_p[kAdp]
 
 R(R0), L(L0), A(A0)
 S = Simulation(R | L | A | C)
 print(S.compile())
-
-
